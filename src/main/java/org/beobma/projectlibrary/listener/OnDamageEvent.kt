1c759e11285f433c8ccf6232b9bcd84bad0d7e64
BEOBMA <dingxiao19@gmail.com>
2024-07-15 08:59:02 +0900

    OnDamageEvent.kt 업데이트

--- src/main/java/org/beobma/projectlibrary/listener/OnDamageEvent.kt
+++ src/main/java/org/beobma/projectlibrary/listener/OnDamageEvent.kt
@@ -11,12 +11,16 @@ class OnDamageEvent : Listener {
     @EventHandler
     fun onPlayerAttack(event: EntityDamageByEntityEvent) {
         val player = event.damager
+        val damage = event.damage
         if (player !is Player) return
         if (!Info.isGaming() && !Info.isStarting()) return
 
         AbnormalStatusManager().run {
             if (!player.isUnableAttack()) return
             event.isCancelled = true
+            return
         }
+
+        
     }
 }
