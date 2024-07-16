b40bd7cd00004e8a5ae566ff40f31bb8fde2d5ac
BEOBMA <dingxiao19@gmail.com>
2024-07-16 10:25:08 +0900

    AbnormalStatusManager.kt 업데이트

--- src/main/java/org/beobma/projectlibrary/abnormalstatus/AbnormalStatusManager.kt
+++ src/main/java/org/beobma/projectlibrary/abnormalstatus/AbnormalStatusManager.kt
@@ -10,65 +10,65 @@ class AbnormalStatusManager {
      * 플레이어에게 이동 불가를 부여함.
      */
     fun Player.addUnableMove() {
-        if (!player!!.isParticipation()) return
+        if (!this.isParticipation()) return
 
-        player!!.scoreboardTags.add("UnableMove")
+        this.scoreboardTags.add("UnableMove")
     }
 
     /**
      * 플레이어가 이동 불가상태인지 여부
      */
     fun Player.isUnableMove(): Boolean {
-        if (!player!!.isParticipation()) return false
+        if (!this.isParticipation()) return false
 
-        return player!!.scoreboardTags.contains("UnableMove")
+        return this.scoreboardTags.contains("UnableMove")
     }
 
     /**
      * 플레이어에게 이동 불가를 제거함.
      */
     fun Player.removeUnableMove() {
-        if (!player!!.isParticipation()) return
+        if (!this.isParticipation()) return
 
-        player!!.scoreboardTags.remove("UnableMove")
+        this.scoreboardTags.remove("UnableMove")
     }
 
     /**
      * 플레이어에게 공격 불가를 부여함.
      */
     fun Player.addUnableAttack() {
-        if (!player!!.isParticipation()) return
+        if (!this.isParticipation()) return
 
-        player!!.scoreboardTags.add("UnableAttack")
+        this.scoreboardTags.add("UnableAttack")
     }
 
     /**
      * 플레이어가 공격 불가상태인지 여부
      */
     fun Player.isUnableAttack(): Boolean {
-        if (!player!!.isParticipation()) return false
+        if (!this.isParticipation()) return false
 
-        return player!!.scoreboardTags.contains("UnableAttack")
+        return this.scoreboardTags.contains("UnableAttack")
     }
 
     /**
      * 플레이어에게 공격 불가를 제거함.
      */
     fun Player.removeUnableAttack() {
-        if (!player!!.isParticipation()) return
+        if (!this.isParticipation()) return
 
-        player!!.scoreboardTags.remove("UnableAttack")
+        this.scoreboardTags.remove("UnableAttack")
     }
 
     /**
      * 플레이어에게 흐트러짐 상태를 부여함.
      */
     fun Player.addDisheveled() {
-        if (!player!!.isParticipation()) return
+        if (!this.isParticipation()) return
 
-        player!!.scoreboardTags.add("Disheveled")
-        player!!.addUnableMove()
-        player!!.addUnableAttack()
+        this.scoreboardTags.add("Disheveled")
+        this.addUnableMove()
+        this.addUnableAttack()
 
         object : BukkitRunnable() {
             override fun run() {
@@ -83,19 +83,19 @@ class AbnormalStatusManager {
      * 플레이어가 흐트러짐 상태인지 여부
      */
     fun Player.isDisheveled(): Boolean {
-        if (!player!!.isParticipation()) return false
+        if (!this.isParticipation()) return false
 
-        return player!!.scoreboardTags.contains("Disheveled")
+        return this.scoreboardTags.contains("Disheveled")
     }
 
     /**
      * 플레이어에게 흐트러짐 상태를 제거함.
      */
     fun Player.removeDisheveled() {
-        if (!player!!.isParticipation()) return
+        if (!this.isParticipation()) return
 
-        player!!.scoreboardTags.remove("Disheveled")
-        player!!.removeUnableMove()
-        player!!.removeUnableAttack()
+        this.scoreboardTags.remove("Disheveled")
+        this.removeUnableMove()
+        this.removeUnableAttack()
     }
 }
